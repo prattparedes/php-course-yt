906c877a30dc8a13aec7b9def9ed02e115583a23
Pratt <pratt.paredes@gmail.com>
2023-01-14 22:55:29 -0500

    html in php

--- site.php
+++ site.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>HOLA AMIGUITOS</h1>
-    <?php 
-        echo('Hello World')
-
+    <?php
+    echo 'Hello World';
+    echo "<h1>Pratt's Site</h1>";
+    echo "<p>This is my site </p>"
     ?>
     <button>RAAAAAAAAAAAA</button>
 </body>
+
 </html>
