440c775c26c59903a6f9813a5466cc3401efd71e
Pratt <pratt.paredes@gmail.com>
2023-01-15 01:38:21 -0500

    str functions

--- site.php
+++ site.php
@@ -13,9 +13,31 @@
     <?php
     echo 'Hello World';
     echo "<h1>Pratt's Site</h1>";
-    echo "<p>This is my site </p>"
+    echo "<hr>";
+    echo "<p>This is my site </p>";
+
+    $characterName = "Pratt";
+    $characterAge = 23;
+    echo "Probando variables en php, nombre: $characterName, edad: $characterAge <br>";
+    $characterName = "Miauri";
+    echo "Probando variables en php, nombre: $characterName, edad: $characterAge <br>";
+    $characterAge = 26;
+    echo "Probando variables en php, nombre: $characterName, edad: $characterAge <br>";
+
+    echo '<hr>';
+
+    $phrase2 = 'Giraffe Academy';
+    echo strtolower($phrase2);
+    echo strtoupper($phrase2);
+    echo strlen($phrase2);
+    echo $phrase2[0];
+    echo $phrase2[4];
+    echo $phrase2[8];
+    // $phrase2[2] = "X";
+    echo "<hr>";
+    echo str_replace("Giraffe", "Panda", $phrase2)
+
     ?>
-    <button>RAAAAAAAAAAAA</button>
 </body>
 
 </html>
